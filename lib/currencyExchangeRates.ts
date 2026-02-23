97d8dbd36d64a2405a483cb08f68bc855e667dae
Marcel <marcelrg.business@gmail.com>
2026-02-23 22:34:07 +0700

    Temporarily fix TS errors

--- lib/currencyExchangeRates.ts
+++ lib/currencyExchangeRates.ts
@@ -2,10 +2,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export async function getExchangeRatePair(base: ExchangeAPICurrencyCode, target: ExchangeAPICurrencyCode): CurrencyExchangePairData {
-
+export async function getExchangeRatePair(base: ExchangeAPICurrencyCode, target: ExchangeAPICurrencyCode): Promise<CurrencyExchangePairData> {
+    base;
+    target;
+    throw new Error;
 }
 
-export async function getExchangeRates(base: ExchangeAPICurrencyCode): CurrencyExchangeData {
-    
+export async function getExchangeRates(base: ExchangeAPICurrencyCode): Promise<CurrencyExchangeData> {
+    base;
+    throw new Error;
 }
