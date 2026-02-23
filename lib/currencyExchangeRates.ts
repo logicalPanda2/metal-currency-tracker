0ee52c59314afed451663166c6d0281059691b21
Marcel <marcelrg.business@gmail.com>
2026-02-23 22:27:34 +0700

    Improve clarity of type names; declare pair function

---
+++ lib/currencyExchangeRates.ts
@@ -0,0 +1,7 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export async function getExchangeRatePair(base: ExchangeAPICurrencyCode, target: ExchangeAPICurrencyCode): CurrencyExchangePairData {
+
+}
