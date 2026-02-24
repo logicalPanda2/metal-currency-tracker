d5adccd3eeb80aa2589ad40eaf57141d50beac9d
Marcel <marcelrg.business@gmail.com>
2026-02-25 04:55:38 +0700

    Adjust focus outline width

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -2,7 +2,7 @@ export default function Button({onClick, styles, textContent}: ButtonProps) {
     return (
         <button 
             onClick={onClick}
-            className={`px-3 py-1 [box-shadow:0_1.5px_2px_0_rgb(20,20,20)] rounded ${styles} hover:bg-neutral-100 active:bg-neutral-200 focus-visible:outline-1 transition`}
+            className={`px-3 py-1 [box-shadow:0_1.5px_2px_0_rgb(20,20,20)] rounded ${styles} hover:bg-neutral-100 active:bg-neutral-200 focus-visible:outline-1.5 transition`}
         >
             {textContent}
         </button>
--- app/components/TextInput.tsx
+++ app/components/TextInput.tsx
@@ -6,7 +6,7 @@ export default function TextInput({name, id, value, onChange, styles}: TextInput
             id={id}
             value={value}
             onChange={onChange}
-            className={`py-1 px-2 rounded ${styles} focus-visible:outline-1 [box-shadow:0_1.5px_2px_0_rgb(20,20,20)] transition`}
+            className={`py-1 px-2 rounded ${styles} focus-visible:outline-1.5 [box-shadow:0_1.5px_2px_0_rgb(20,20,20)] transition`}
             autoComplete="false"
         />
     );
