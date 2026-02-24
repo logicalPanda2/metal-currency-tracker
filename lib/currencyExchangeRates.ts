58048d3b93214809702aaed10501ebf7b168a4fb
Marcel <marcelrg.business@gmail.com>
2026-02-24 23:27:17 +0700

    Edit types

--- lib/currencyExchangeRates.ts
+++ lib/currencyExchangeRates.ts
@@ -2,17 +2,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export async function getExchangeRatePair(
-	base: ExchangeAPICurrencyCode,
-	target: ExchangeAPICurrencyCode,
+export async function getCurrencyExchangePair(
+	base: CurrencyAPICurrencyCode,
+	target: CurrencyAPICurrencyCode,
 ): Promise<CurrencyExchangePairData> {
 	base;
 	target;
 	throw new Error();
 }
 
-export async function getExchangeRates(
-	base: ExchangeAPICurrencyCode,
+export async function getCurrencyExchangeAll(
+	base: CurrencyAPICurrencyCode,
 ): Promise<CurrencyExchangeData> {
 	base;
 	throw new Error();
