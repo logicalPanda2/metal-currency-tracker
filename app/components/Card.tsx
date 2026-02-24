b1546d6a0a8d661836e523a2ea500cf7788750cb
Marcel <marcelrg.business@gmail.com>
2026-02-25 04:56:31 +0700

    Add  shadow to card

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,6 +1,6 @@
 export default function Card({mainText, subText}: CardProps) {
     return (
-        <div className="p-4 rounded border inline-block">
+        <div className="p-4 rounded inline-block [box-shadow:0_1.5px_2.5px_0_rgb(20,20,20)]">
             <p className="text-2xl mb-0.5">{mainText}</p>
             <p className="text-neutral-900">{subText}</p>
         </div>
