c58c656717e7b8f10128c7f298d143f4ce7dac9f
Marcel <marcelrg.business@gmail.com>
2026-02-24 23:52:13 +0700

    Add currency pair fetch

--- lib/currencyExchangeRates.ts
+++ lib/currencyExchangeRates.ts
@@ -6,9 +6,34 @@ export async function getCurrencyExchangePair(
 	base: CurrencyAPICurrencyCode,
 	target: CurrencyAPICurrencyCode,
 ): Promise<CurrencyExchangePairData> {
-	base;
-	target;
-	throw new Error();
+	try {
+		if (!process.env.API_KEY_SECRET_2)
+			throw new Error("API access key missing in environment variables.");
+
+        if(!process.env.API_ENDPOINT_2A || !process.env.API_ENDPOINT_2B)
+            throw new Error("API not configured in environment variables.");
+
+        const endpoint = process.env.API_ENDPOINT_2A;
+
+		const response = await fetch(
+			endpoint.replace("KEY", process.env.API_KEY_SECRET_2).replace("BASE", base).replace("TARGET", target),
+		);
+
+		if (!response.ok)
+			throw new Error(`${response.status} ${response.statusText}`);
+
+		const data: CurrencyAPIPairResponse = await response.json();
+
+		const processed: CurrencyExchangePairData = {
+            base: data.base_code,
+            target: data.target_code,
+            rate: data.conversion_rate,
+        }
+
+		return processed;
+	} catch (e) {
+		throw e;
+	}
 }
 
 export async function getCurrencyExchangeAll(
